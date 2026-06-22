ebb68ea1dbc52a505a0d2081eeffd1f84d80079a
mpopov <mpopov@mpopov.com>
2026-06-22 11:00:12 -0400

    pipe webhook trigger logs to container console so it can be seen in argocd. use rsync instead of cp to copy files

--- docu_container/redeploy.sh
+++ docu_container/redeploy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Sends echo output to primary console stream of container
+echo "==================" > /proc/1/fd/1
+echo "Webhook triggered!" > /proc/1/fd/1
+echo "==================" > /proc/1/fd/1
+
 # pull from repo when webhook triggers, rebuild html files, and copy to shared volume for caddy
 cd /quartz
 git -C /quartz/content pull
 npx quartz build --port 8080
-cp -r public/* shared/
+rsync --delete --recursive public/ shared/
