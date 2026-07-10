e195fcca3550415ca5a90e5a79d2760347113b52
mpopov <mpopov@mpopov.com>
2026-07-10 12:01:51 -0400

    working version of dns_copy. basic tests passed. still needs better logging

--- Projects/dns/dns_copy.py
+++ Projects/dns/dns_copy.py
@@ -1,4 +1,4 @@
-import requests, os, dns.resolver, sqlite3, time
+import requests, os, dns.resolver, time
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.dns import DnsManagementClient
 from dotenv import load_dotenv
@@ -7,17 +7,6 @@ load_dotenv()
 technitium_api_token = os.getenv("technitium_api_token")
 subscription_id = os.getenv("subscription_id")
 
-# SQLITE DB SETUP
-con = sqlite3.connect("/home/mpopov/testrepo/Projects/dns/bla.db")
-cur = con.cursor()
-cur.execute("""CREATE TABLE IF NOT EXISTS azure (
-    record_fqdn TEXT,
-    record_type TEXT,
-    record_value TEXT,
-    UNIQUE(record_fqdn, record_type, record_value)
-)
-""")
-
 # DNS RESOLVER
 custom_resolver = dns.resolver.Resolver()
 
@@ -25,17 +14,17 @@ def get_azure_records():
     """get iterable object containing record sets from a zone from Azure"""
     dns_client = DnsManagementClient(DefaultAzureCredential(), subscription_id)
     record_sets = dns_client.record_sets.list_by_dns_zone(
-        resource_group_name=resource_group_name,
-        zone_name=zone
+        resource_group_name=RESOURCE_GROUP_NAME,
+        zone_name=ZONE
         )
     return record_sets
 
 def get_technitium_records():
     """get dict containing all records from a zone from Technitium"""
-    url_get = f"http://{technitium_ip}:5380/api/zones/records/get"
+    url_get = f"http://{TECHNITIUM_IP}:5380/api/zones/records/get"
     params = {
             "token": technitium_api_token,
-            'domain':zone,
+            'domain':ZONE,
             'listZone':True
     }
     return requests.get(url_get,params=params).json()
@@ -48,7 +37,7 @@ class Record:
         # record parameters for technitium
         self.params = {
             "token":technitium_api_token,
-            'zone':zone,
+            'zone':ZONE,
             'domain':self.record_fqdn,
             'type':self.record_type,
         }
@@ -65,47 +54,17 @@ class Record:
             return
         for value in answer:
             self.record_value.append(value.to_text()) # .to_text() is needed
-            self.record_value.sort()
-
-    def add_record_to_table(self, record_value):
-        cur.execute(
-            """
-            INSERT OR IGNORE INTO azure (record_fqdn, record_type, record_value)
-            VALUES (?, ?, ?)
-            """, 
-            (self.record_fqdn, self.record_type, record_value)
-        )
-        con.commit()
-
-    def record_exists_in_table(self, record_value):
-        cur.execute(
-            """
-            SELECT 1
-            FROM azure
-            WHERE record_fqdn = ?
-            AND record_type = ?
-            AND record_value = ?
-            LIMIT 1
-            """,
-            (self.record_fqdn, self.record_type, record_value)
-        )
-        if cur.fetchone() is None:
-            print(f"Does not match: {self.record_fqdn} {self.record_type} {record_value}")
-            return False
-        else:
-            return True
+        self.record_value.sort()
     
     def add_values_to_technitium(self):
-        url_add = f"http://{technitium_ip}:5380/api/zones/records/add"
-        self.api_call(url_add)
+        url_add = f"http://{TECHNITIUM_IP}:5380/api/zones/records/add"
+        requests.get(url_add, params=self.params)
+        print(f"adding {self.record_fqdn}")
 
     def del_values_from_technitium(self):
-        url_del = f"http://{technitium_ip}:5380/api/zones/records/delete"
-        self.api_call(url_del)
+        url_del = f"http://{TECHNITIUM_IP}:5380/api/zones/records/delete"
+        requests.get(url_del, params=self.params)
         print(f"deleting {self.record_fqdn}")
-
-    def api_call(self,url):
-        requests.get(url, params=self.params)
 
 class A(Record):
     def build_params(self,record_value):
@@ -127,7 +86,7 @@ class TXT(Record):
             if '" "' in value: # dnspython txt record, split into list
                 value = value.replace('" "', '')
             self.record_value[index] = value
-        # convert list to a string, technitium will handle the rest
+        # convert list to a string
         string = "\n".join(self.record_value)
 
         # adding new lines after the 255 byte limit for splitText param
@@ -147,7 +106,7 @@ class TXT(Record):
 
     def get_record_value(self,nameserver):
         super().get_record_value(nameserver)
-        if nameserver == technitium_ip:
+        if nameserver == TECHNITIUM_IP:
             self.format_technitium_record_value()
         else:
             self.format_azure_record_value()
@@ -158,9 +117,6 @@ class CAA(Record):
         self.params['flags'] = flags
         self.params['tag'] = tag
         self.params['value'] = value.strip('"')
-
-    def get_record_value(self,nameserver):
-        super().get_record_value(nameserver)
 
 class SRV(Record):
     def build_params(self,record_value):
@@ -176,20 +132,20 @@ class MX(Record):
         self.params['exchange'] = exchange
         self.params['preference'] = preference
 
-def main():
-    global zone, resource_group_name, technitium_ip
-    zone = "kenwavesolutions.com"
-    resource_group_name = "Pipesonik_Resources"
-    technitium_ip = "192.168.88.2"
-    
+def test_main():
+    global ZONE, RESOURCE_GROUP_NAME, TECHNITIUM_IP
+    ZONE = "kenwavesolutions.com"
+    RESOURCE_GROUP_NAME = "Pipesonik_Resources"
+    TECHNITIUM_IP = "192.168.88.2"
     RECORD_CLASSES = { "A": A, "CNAME": CNAME, "TXT": TXT, "CAA": CAA, "SRV": SRV, "MX": MX,}
 
-    loop = 0
+    loop_count = 0
+
     while True:
 
-        cur.execute("DELETE FROM azure")
-        con.commit()
-
+        azure_records_now = set()
+        technitium_records_now = set()
+        technitium_records = get_technitium_records()
         azure_records = get_azure_records()
 
         # Initializing the recordsets (not a record yet)
@@ -202,21 +158,11 @@ def main():
             else:
                 new_record = record_class(record_type,record_fqdn)
             
-            # Obtaining record data
             new_record.get_record_value("8.8.8.8") # gets recordset data, not per record
 
             # this for-loop processes data per record, NOT recordset like above
             for record_value in new_record.record_value:
-                # updates object's params/values temporarily to perform actions
-                new_record.build_params(record_value)
-                new_record.add_values_to_technitium()
-                new_record.add_record_to_table(record_value)
-        
-        print("-"*40)
-        print("-"*40)
-        print("-"*40)
-        
-        technitium_records = get_technitium_records()
+                azure_records_now.add((record_fqdn,record_type,record_value))    
 
         for record in technitium_records["response"]["records"]:
             record_type = record['type']
@@ -230,68 +176,26 @@ def main():
             new_record.get_record_value("192.168.88.2")
 
             for record_value in new_record.record_value:
-                new_record.build_params(record_value)
-                if new_record.record_exists_in_table(record_value):
-                    new_record.add_values_to_technitium()
-                else:
-                    new_record.del_values_from_technitium()
+                technitium_records_now.add((record_fqdn,record_type,record_value))
 
-        loop += 1
-        print(f"loop number {loop} completed")
-        time.sleep(10)
-
-def test_main():
-    global zone, resource_group_name, technitium_ip
-    zone = "kenwavesolutions.com"
-    resource_group_name = "Pipesonik_Resources"
-    technitium_ip = "192.168.88.2"
-    
-    RECORD_CLASSES = { "A": A, "CNAME": CNAME, "TXT": TXT, "CAA": CAA, "SRV": SRV, "MX": MX,}
-
-    azure_records = get_azure_records()
-    azure_records_now = set()
-    technitium_records_now = set()
-
-    # Initializing the recordsets (not a record yet)
-    for record in azure_records:
-        record_type = record.type.split("/")[-1]
-        record_fqdn = record.fqdn.strip(".")
-        record_class = RECORD_CLASSES.get(record_type)
-        if record_class is None:
-            continue
-        else:
-            new_record = record_class(record_type,record_fqdn)
+        records_to_add = azure_records_now - technitium_records_now
         
-        # Obtaining record data
-        new_record.get_record_value("8.8.8.8") # gets recordset data, not per record
-
-        # this for-loop processes data per record, NOT recordset like above
-        for record_value in new_record.record_value:
-            # updates object's params/values temporarily to perform actions
-            azure_records_now.add((record_fqdn,record_type,record_value))
-        
-    technitium_records = get_technitium_records()
-
-    for record in technitium_records["response"]["records"]:
-        record_type = record['type']
-        record_fqdn = record['name']
-        record_class = RECORD_CLASSES.get(record_type)
-        if record_class is None:
-            continue
-        else:
+        for record_fqdn, record_type, record_value in records_to_add:
+            record_class = RECORD_CLASSES.get(record_type)
             new_record = record_class(record_type,record_fqdn)
-
-        new_record.get_record_value("192.168.88.2")
-
-        for record_value in new_record.record_value:
             new_record.build_params(record_value)
-            technitium_records_now.add((record_fqdn,record_type,record_value))
+            new_record.add_values_to_technitium()
 
-    x = azure_records_now - technitium_records_now
-    y = technitium_records_now - azure_records_now
-    print(x)
-    print('-'*88)
-    print(y)
-    
+        record_to_delete = technitium_records_now - azure_records_now
+
+        for record_fqdn, record_type, record_value in record_to_delete:
+            record_class = RECORD_CLASSES.get(record_type)
+            new_record = record_class(record_type,record_fqdn)
+            new_record.build_params(record_value)
+            new_record.del_values_from_technitium()
+
+        loop_count += 1
+        print(f"Current loop count: {loop_count}")
+        time.sleep(10)
 
 test_main()
